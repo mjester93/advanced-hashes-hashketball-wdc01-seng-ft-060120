2906c266649b51605eb910b638e6f4325f2dc502
Michael Jester <m.jester.93@gmail.com>
2020-05-23 06:53:39 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # Write your code below game_hash
 def game_hash
   {
@@ -127,3 +129,194 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored(player_name)
+  game_hash[:home][:players].each do |x, values|
+    if x[:player_name] == player_name
+      return x[:points]
+    end
+  end
+
+  game_hash[:away][:players].each do |x, values|
+    if x[:player_name] == player_name
+      return x[:points]
+    end
+  end
+end
+
+
+def shoe_size(player_name)
+  game_hash[:home][:players].each do |x, values|
+    if x[:player_name] == player_name
+      return x[:shoe]
+    end
+  end
+
+  game_hash[:away][:players].each do |x, values|
+    if x[:player_name] == player_name
+      return x[:shoe]
+    end
+  end
+end
+
+
+def team_colors(team_name)
+  if game_hash[:home][:team_name] == team_name
+    return game_hash[:home][:colors]
+  else
+    return game_hash[:away][:colors]
+  end
+end
+
+
+def team_names
+  return [
+    game_hash[:home][:team_name],
+    game_hash[:away][:team_name]
+  ]
+end
+
+def player_numbers(team_name)
+  numbers = []
+
+  if game_hash[:home][:team_name] == team_name
+    game_hash[:home][:players].each do |x, values|
+      numbers.push(x[:number])
+    end
+  else
+    game_hash[:away][:players].each do |x, values|
+      numbers.push(x[:number])
+    end
+  end
+
+  return numbers
+end
+
+
+def player_stats(player_name)
+  game_hash[:home][:players].each do |x, values|
+    if x[:player_name] == player_name
+      return x
+    end
+  end
+
+  game_hash[:away][:players].each do |x, values|
+    if x[:player_name] == player_name
+      return x
+    end
+  end
+end
+
+
+def big_shoe_rebounds
+  max_shoe_size = -99999
+  max_shoe_size_name = ""
+
+  game_hash[:home][:players].each do |x, values|
+    if x[:shoe] >= max_shoe_size
+      max_shoe_size = x[:shoe]
+      max_shoe_size_name = x[:player_name]
+    end
+  end
+
+  game_hash[:away][:players].each do |x, values|
+    if x[:shoe] >= max_shoe_size
+      max_shoe_size = x[:shoe]
+      max_shoe_size_name = x[:player_name]
+    end
+  end
+
+  puts max_shoe_size_name
+  return player_stats(max_shoe_size_name)[:rebounds]
+end
+
+
+def most_points_scored
+  max_points = -99999
+  max_points_name = ""
+
+  game_hash[:home][:players].each do |x, values|
+    if x[:points] >= max_points
+      max_points = x[:points]
+      max_points_name = x[:player_name]
+    end
+  end
+
+  game_hash[:away][:players].each do |x, values|
+    if x[:points] >= max_points
+      max_points = x[:points]
+      max_points_name = x[:player_name]
+    end
+  end
+
+  return max_points_name
+end
+
+
+def winning_team
+  home_score = 0
+  away_score = 0
+
+  game_hash[:home][:players].each do |x, values|
+    home_score += x[:points]
+  end
+
+  game_hash[:away][:players].each do |x, values|
+    away_score += x[:points]
+  end
+
+  if home_score > away_score
+    return game_hash[:home][:team_name]
+  elsif away_score > home_score
+    return game_hash[:away][:team_name]
+  else
+    return "TIE"
+  end
+end
+
+
+def player_with_longest_name
+  max_letters = 0
+  max_letters_name = ""
+
+  game_hash[:home][:players].each do |x, values|
+    if x[:player_name].length >= max_letters
+      max_letters = x[:player_name].length
+      max_letters_name = x[:player_name]
+    end
+  end
+
+  game_hash[:away][:players].each do |x, values|
+    if x[:player_name].length >= max_letters
+      max_letters = x[:player_name].length
+      max_letters_name = x[:player_name]
+    end
+  end
+
+  return max_letters_name
+end
+
+
+def long_name_steals_a_ton?
+  max_steals = -99999
+  max_steals_name = ""
+
+  game_hash[:home][:players].each do |x, values|
+    if x[:steals] >= max_steals
+      max_steals = x[:points]
+      max_steals_name = x[:player_name]
+    end
+  end
+
+  game_hash[:away][:players].each do |x, values|
+    if x[:steals] >= max_steals
+      max_steals = x[:points]
+      max_steals_name = x[:player_name]
+    end
+  end
+
+  if max_steals_name == player_with_longest_name
+    return true
+  else
+    return false
+  end
+end
