f3689aa019611b70d72407fed192cfbc51c491c6
Michael Jester <m.jester.93@gmail.com>
2020-05-23 06:31:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -137,7 +137,6 @@ def num_points_scored(player_name)
   end
 
   game_hash[:away][:players].each do |x, values|
-    binding.pry
     if x[:player_name] == player_name
       return x[:points]
     end
