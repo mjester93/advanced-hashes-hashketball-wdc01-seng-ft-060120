8db2e578d8fe799f2e81f26ddd4ea1da53564f19
Michael Jester <m.jester.93@gmail.com>
2020-05-23 06:44:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -228,3 +228,26 @@ def big_shoe_rebounds
   puts max_shoe_size_name
   return player_stats(max_shoe_size_name)[:rebounds]
 end
+
+
+def most_points_scored
+  max_points = -99999
+  max_points_name = ""
+
+  game_hash[:home][:players].each do |x, values|
+    if x[:shoe] >= max_shoe_size
+      max_shoe_size = x[:shoe]
+      max_shoe_size_name = x[:player_name]
+    end
+  end
+
+  game_hash[:away][:players].each do |x, values|
+    if x[:shoe] >= max_shoe_size
+      max_shoe_size = x[:shoe]
+      max_shoe_size_name = x[:player_name]
+    end
+  end
+
+  puts max_shoe_size_name
+  return player_stats(max_shoe_size_name)[:rebounds]
+end
